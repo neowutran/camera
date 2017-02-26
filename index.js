0222d978974069982d9241a956e97b5f4a770882
Neowutran <me@neowutran.ovh>
2017-02-26 13:19:53 +0100

    some fix

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
-function camera(dispatch){
-	console.log("new camera instance");
+var server = false;
+function camera(){
+	
 	let cameraAngle = 0;
-
+	if(server != false){
+		server.close();
+	}
+	
 	app.get('/camera', function (req, res) {
 		cameraAngle = req.query.angle;
 		res.end( "" );
-	})
-
-	var server = app.listen(9999, function () {
+	});
+	
+	server = app.listen(9999, function () {
 		var host = server.address().address
 		var port = server.address().port
 		console.log("Listening camera modification at http://%s:%s", host, port)
@@ -25,6 +28,8 @@ function camera(dispatch){
 let instance = false;
 
 module.exports = function NpcManager(dispatch){
-    if(!dispatch) return instance;
-    instance = new camera(dispatch);
+    if(instance == false){
+		instance = new camera();
+	}
+	return instance;
 }
